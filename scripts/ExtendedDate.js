8829c7f218fdeab242a3eb58602681c672302035
prikkeldraad <prikkeldraad@gmail.com>
2018-04-06 07:33:14 +0200

    Added some libs and tests

--- scripts/ExtendedDate.js
+++ scripts/ExtendedDate.js
@@ -16,8 +16,45 @@ class ExtendedDate extends Date {
 	 * @return {number} beats since midnight in Zurich
 	 */
 	getBeats() {
-		var fraction = 1000 / 84600;
-		alert(fraction);
+		var fraction = 1000 / 86400;
 		return this.getTotalSeconds() * fraction;
 	}
+	
+	/**
+	 * Octal time
+	 */
+}
+
+/**
+ * Hexadecimal time
+ * 
+ * A day is 1 unit. The day begins at midnight at .0000. Each increment is
+ * a second. So .8000 is noon and midnight is .FFFF
+ */
+class HexadecimalTime extends ExtendedDate {
+	//constructor(year=null, month=null, day=null, hour=null, minutes=null, seconds=null, milliseconds=null) {
+		//if (day != null) {
+			//super(year, month, day, hour, minutes, seconds, milliseconds);
+		//}
+		//this.fraction = 65536 / 86400;*/
+	//}
+	
+	getHexTime() {
+		var sec = this.getTotalSeconds() * 65536 / 86400;
+		return "." + ("0000" + this.dec2hex(sec)).slice(-4);
+	}
+	
+	getIntuitorHexTime() {
+		var time = this.getHexTime();
+		return time.substr(1, 1) + "_" + time.substr(2,2) + "_" + time.substr(4,1);
+	}
+	
+	hex2dec(hex) {
+		return parseInt(hex, 16);
+	}
+	
+	dec2hex(dec) {
+		return Number(dec).toString(16).toUpperCase();
+	}
+	
 }
--- tests/ExtendedDate.js
+++ tests/ExtendedDate.js
@@ -27,3 +27,29 @@ QUnit.test( "getBeats", function ( assert) {
 	beats = now.getBeats();
 	assert.equal(beats, 500, "Beats at noon should be 500");
 });
+
+QUnit.test( "hex2dec", function (assert) {
+	var hex = new HexadecimalTime();
+	assert.equal(hex.hex2dec('BB'), 187, "BB in hex equals 187 in dec");
+	assert.equal(hex.dec2hex('255'), 'FF', "255 in dec equals FF in hex");
+});
+
+QUnit.test("hextime", function (assert) {
+	var hex = new HexadecimalTime(2000, 1, 1);
+	assert.equal(hex.getHexTime(), ".0000", "Hextime is .0000 at midnight");
+	
+	var hex = new HexadecimalTime(2000, 1, 1, 12);
+	assert.equal(hex.getHexTime(), ".8000", "Hextime should be .8000 at noon");
+});
+
+QUnit.test("getIntuitorHexTime", function (assert) {
+	var hex = new HexadecimalTime(2000, 1, 1);
+	assert.equal(hex.getIntuitorHexTime(), "0_00_0", "IntuitorHexTime is 0_00_0 at midnight");
+	
+	var hex = new HexadecimalTime(2000, 1, 1, 12);
+	assert.equal(hex.getIntuitorHexTime(), "8_00_0", "IntuitorHexTime should be 8_00_0 at noon");
+
+	var hex = new HexadecimalTime(2000, 1, 1, 23, 15);
+	assert.equal(hex.getIntuitorHexTime(), "F_80_0", "IntuitorHexTime should be F_80_0 at 23:15");
+
+});
